8547d9ed311b8543deba53243c1cb56ac2a3708d
imadimad1996 <imadazizimad@gmail.com>
2026-07-18 14:35:48 +0100

    feat: implement server-side request middleware for hostname-based routing and redirection

---
+++ .wrangler/tmp/pages-Pe8WyA/functionsRoutes-0.8199650704986625.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ___middleware_ts_onRequest } from "C:\\Users\\DeLL\\CARECALCULUS\\functions\\_middleware.ts"
+
+export const routes = [
+    {
+      routePath: "/",
+      mountPath: "/",
+      method: "",
+      middlewares: [___middleware_ts_onRequest],
+      modules: [],
+    },
+  ]
