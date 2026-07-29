23c9ddf9067ca79f86cd87c999c868d3f176f322
imadimad1996 <imadazizimad@gmail.com>
2026-07-29 16:32:55 +0100

    fix: improve service worker cache handling by adding asset error detection and enabling workbox lifecycle configurations

--- vite.config.ts
+++ vite.config.ts
@@ -46,6 +46,9 @@ export default defineConfig(() => {
           ]
         },
         workbox: {
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+          skipWaiting: true,
           globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
           runtimeCaching: [
             {
